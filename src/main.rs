0e79371134fff2ff88b9f2a0ce3b79bcdcf1e79a
Paul Jickling <pjickling@gmail.com>
2019-03-02 14:25:57 -0800

    Outputs map generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let generate = Command::new("src/generate/proc.o")
+        .output()
+        .expect("failed to execute process");
+
+    let mut files = String::new();
+    for c in generate.stdout.as_slice() {
+        if c.is_ascii() == true {
+            files.push(*c as char);
+        }
+    }
+    println!("{}", files);
 }
