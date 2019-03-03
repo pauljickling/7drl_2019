796b7130011d9723472b630cb395fda509911e9d
Paul Jickling <pjickling@gmail.com>
2019-03-02 20:22:02 -0800

    Added intro text to game

---
+++ src/txt.rs
@@ -0,0 +1,6 @@
+/// Text files for the game
+
+_intro_text = "
+Back when human societies were shepharded by the spirit world they lived through population boom & bust cycles. Demonic spirits kept the population in check through famine, war, and human sacrifice. Humans, though short-lived and stupid, had occasional moments of genuine cleverness. After centuries of servitude under the domain of the spirits, humans gradually learned the dark arts of necromancy. They took dead, inanimate matter from the earth and transformed it into energy that made them wildly powerful. The spirit world, deprived of the ancient anima from within the earth, receded away from existence. This was called the technological age, and the humans ruled over the earth in a kingdom of death. Spiritual values decayed as humanity offered nothing but its empty, self-regard. 
+
+However the technological age did not last. The humans embraced a philosophy of selfishness at a time when they had become the guardians of earth. Rather than protect the life forces of their planet they poisoned the air, water, and earth. As the planet became uninhabitable, the human societies collapsed, unable to forge a new philosophy of substantive collaboration. As the humans collapsed, the spirits slowly returned to the world, and punished the humans viciously."
